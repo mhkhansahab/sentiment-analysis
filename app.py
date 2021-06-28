aca0229832fb3262cfaa3e22c4e4eb2aa77059d5
Muhammad Hamza Khan <hamza.khansahab@gmail.com>
2021-06-29 02:11:23 +0500

    minor changes

--- app.py
+++ app.py
@@ -20,5 +20,7 @@ def sentimentAnalyzer():
       }
       return dict
 
+port = int(os.environ.get('PORT', 8000))
+
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=port)
